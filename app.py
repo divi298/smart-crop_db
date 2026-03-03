07735a9d7c90cc227ef01cc7978a9c1b0c653ce5
Divija <23b01a4280@svecw.edu.in>
2026-03-03 21:31:47 +0530

    changes in main code

--- app.py
+++ app.py
@@ -236,5 +236,5 @@ def speak():
 # 🚀 Render Production Config
 # ==============================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
